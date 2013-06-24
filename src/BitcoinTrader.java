cac085519e824e77d84649be6742dd0fea2672a1
Sam Watson <sam@toomuch.com.au>
2013-06-25 02:28:43 +0800

    Removed recording of redundant data and cleaned the output file

--- src/BitcoinTrader.java
+++ src/BitcoinTrader.java
@@ -3,8 +3,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.TimeZone;
@@ -54,16 +56,69 @@ public class BitcoinTrader {
 			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 			dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 			
+			double previousPrice = 0;
+			
 			while (true) {
 				double price = trade.getLastPriceUSD();
-				Date date = new Date();
-				String dateString = dateFormat.format(date);
-				System.out.println("$" + price + " at " + dateString);
-				writer.write("\"" + dateString + "\", \"" + price + "\"\n");
-				writer.flush();
+				
+				if (price != previousPrice) {
+					Date date = new Date();
+					String dateString = dateFormat.format(date);
+					System.out.println("$" + price + " at " + dateString);
+					writer.write("\"" + dateString + "\", \"" + price + "\"\n");
+					writer.flush();
+					previousPrice = price;
+				} else {
+					System.out.println("$" + price + " is same as previous.");
+				}
+				
 				Thread.sleep(15000);
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void cleanDataFile(String filename) {
+		try {
+			Scanner scanner = new Scanner(new FileInputStream(filename), encoding);
+			
+			ArrayList<String> outputLines = new ArrayList<String>();
+			outputLines.add(scanner.nextLine());
+			
+			double previousPrice = 0;
+			
+			int totalLines = 0;
+			int removedLines = 0;
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				double price = Double.parseDouble(line.split(",")[1].replaceAll("\"", ""));
+				if (price != previousPrice) {
+					outputLines.add(line);
+					System.out.println(line);
+					previousPrice = price;
+				} else {
+					removedLines++;
+				}
+				totalLines++;
+			}
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
+			
+			for (int i = 0; i < outputLines.size(); i++) {
+				writer.write(outputLines.get(i));
+			}
+			
+			writer.flush();
+			writer.close();
+			
+			System.out.println("Removed " + (((double)removedLines / (double)totalLines) * 100) + "% of lines (" + removedLines + "/" + totalLines + ")");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
